c1ef7105f72899f9d5b6d954dab9f1987d40fa74
Murat Ateş <mrtt.atss@gmail.com>
2024-11-21 21:03:43 +0300

    updated

---
+++ Consumer.NotificationService/Program.cs
@@ -0,0 +1,45 @@
+using Consumer.NotificationService.Handler;
+using SharedLibrary.Config;
+using SharedLibrary.Config.Data;
+using SharedLibrary.Consumer.Concrete;
+using SharedLibrary.Extension;
+using SharedLibrary.Model;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Env load
+EnvironmentConfig.LoadEnv();
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+builder.Services
+    .AddRabbitMqServices()
+    .AddRabbitMqChannel()
+    .AddRabbitMqMessageHandler<Order, NotificationMessageHandler>()
+    .AddRabbitMqBackgroundService<Order, RabbitMqConsumer<Order>>(
+        queueName: RabbitMqConsumerConfigData.NotificationQueueName ?? string.Empty,
+        exchangeName: RabbitMqExchangeConfigData.OrderExchange ?? string.Empty);
+
+// Swagger/OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Consumer Notification Service API v1");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
